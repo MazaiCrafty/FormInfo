0a0af5b79f81f7c07caf84a7e3585948807c8675
Lamda <takatim1574@icloud.com>
2018-03-30 21:10:55 +0900

    Update Console.php

--- src/jp/mazaicrafty/pmmp/FormInfo/form/Console.php
+++ src/jp/mazaicrafty/pmmp/FormInfo/form/Console.php
@@ -1,19 +1,5 @@
 <?php
 
-/*
- * ___  ___               _ _____            __ _
- * |  \/  |              (_)  __ \          / _| |
- * | .  . | __ _ ______ _ _| /  \/_ __ __ _| |_| |_ _   _
- * | |\/| |/ _` |_  / _` | | |   | '__/ _` |  _| __| | | |
- * | |  | | (_| |/ / (_| | | \__/\ | | (_| | | | |_| |_| |
- * \_|  |_/\__,_/___\__,_|_|\____/_|  \__,_|_|  \__|\__, |
- *                                                   __/ |
- *                                                  |___/
- * Copyright (C) 2017-2018 @MazaiCrafty (https://twitter.com/MazaiCrafty)
- *
- * This program is free plugin.
- */
-
 namespace jp\mazaicrafty\pmmp\FormInfo\form;
 
 # Player
